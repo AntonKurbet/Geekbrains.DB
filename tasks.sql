44e8164c4b04701513860142d0a51478c25d6869
Anton Kurbet <anton.kurbet@gmail.com>
2020-12-24 00:17:56 +0300

    draft

---
+++ tasks.sql
@@ -0,0 +1,45 @@
+-- 1. Создайте запрос, который выведет информацию:
+--  id пользователя;
+-- имя;
+-- лайков получено;
+-- лайков поставлено;
+-- взаимные лайки.
+
+SELECT DISTINCT
+    q1.id_who, q1.name, q1.likes_times, q2.liked_times
+FROM
+    (SELECT 
+        id_who, name, COUNT(*) likes_times
+    FROM
+        likes l
+    JOIN users u ON u.id = l.id_who
+    GROUP BY id_who) q1
+        left JOIN
+    (SELECT 
+        id_whom, name, COUNT(*) liked_times
+    FROM
+        likes l
+    JOIN users u ON u.id = l.id_whom
+    GROUP BY id_whom) q2
+    on q1.id_who = q2.id_whom;
+
+-- 2. Для структуры из задачи 1 выведите список всех пользователей, 
+-- которые поставили лайк пользователям A и B (id задайте произвольно), 
+-- но при этом не поставили лайк пользователю C.
+
+select q1.id_who from 
+(SELECT DISTINCT
+    id_who
+FROM
+    likes
+WHERE
+    id_whom = 1) q1
+    JOIN
+(SELECT DISTINCT
+    id_who
+FROM
+    likes
+WHERE
+    id_whom = 2) q2
+    on q1.id_who = q2.id_who
+    
